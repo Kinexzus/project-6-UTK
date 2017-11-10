c247a46efe222b8c90d61dafd6179b8d70a1c8b0
Kinexzus <33512757+Kinexzus@users.noreply.github.com>
2017-11-10 22:51:58 +0300

    Delete Access_Rights.php

--- Access_Rights.php
+++
@@ -1,9 +0,0 @@
-<?php
-var_dump($_REQUEST);
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo "Сдесь будет реалзован интерфейс для изменения пров доступа к файлам";
