e778b5cad7c4395d0cd0dc42609dd13da2512aad
KiselFool <kiselyauskas@gmail.com>
2017-11-14 00:24:16 +0300

    Add File_System_Work\this_and_that.php this file system functions (while only one)

---
+++ File_System_Work/this_and_that.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Функция возваращает данные о содержимом директории
+ * @param string $__fspath - путь до директории, содержащей файловую систему данных пользователей
+ * @param string $__upath - путь к директории формата '/user/folder/.../otherfolder'
+ * @return array
+ */
+function in_dir($__fspath, $__upath)
+{
+    $path_arr = glob($__fspath . $__upath . '/*');
+
+    $files_arr = [];    //массив файлов
+    $files_arr[0] = $__upath;
+    foreach($path_arr as $path)
+    {
+        $file_info = [];                        //данные о файле:
+        $file_info['type'] = filetype($path);   //тип файла
+        $file_info['name'] = basename($path);   //имя файла
+        $file_info['size'] = filesize($path);   //размер файла
+        //время последней модификации
+        //права дотупа
+
+        $files_arr[] = $file_info;
+    }
+
+    return $files_arr;
+}
