084bd785efad0d50ea3d0c7d6bf0bc5b8f4ec442
Kinexzus <33512757+Kinexzus@users.noreply.github.com>
2017-12-11 22:58:56 +0300

    !

--- Cloud_Interface.php
+++ Cloud_Interface.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
         <?php
         class Cloud_Interface //Интерфес облочного хранилища
         {
@@ -246,5 +235,3 @@ and open the template in the editor.
             } 
         }
         ?>
-    </body>
-</html>
