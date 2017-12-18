c99c5e0b10c2ab7c1b14fa07f201e26a3ea9d1a8
Kinexzus <33512757+Kinexzus@users.noreply.github.com>
2017-12-19 01:10:04 +0300

    вернул стили как было

--- Print/Print.php
+++ Print/Print.php
@@ -1,4 +1,5 @@
 <?php
+
 class _Print //Интерфес облочного хранилища
 {   
     private $__action;
@@ -20,9 +21,11 @@ class _Print //Интерфес облочного хранилища
             $html = '
             <html lang="en" >
             <head>
+				<title>Cloud Storage</title>
               <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			  <link href="Log.css" rel="stylesheet">
-              <title>Cloud Storage</title>
+              <style>
+				'.$this->GetStyleForm().'
+			</style>
 
             </head>
 
@@ -66,9 +69,10 @@ class _Print //Интерфес облочного хранилища
             <head>
 				
              <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			 <link href="Log.css" rel="stylesheet">
               <title>Cloud Storage</title>
-
+				<style>
+				'.$this->GetStyleForm().'
+			</style>
             </head>
 
             <body>
@@ -182,7 +186,45 @@ class _Print //Интерфес облочного хранилища
          <meta charset="UTF-8">
 		 <link href="FSI.css" rel="stylesheet">
          <title>Cloud Storage</title>
+		<style>
+		
+		body{
+			margin: 0;
+			padding: 0;
+			background: #000;
+			color: #fff;
+			font-family: Arial;
+			font-size: 12px;
+			}
+		a {
+			text-decoration: none;
+		  } 
+			 [type=submit]{
+             width: 250px;
+             height: 30px;
+             background: transparent;
+             border: 1px solid rgba(255,255,255,0.6);
+             border-radius: 2px;
+             color: #fff;
+             font-family: ."Exo"., sans-serif;
+             font-size: 16px;
+             font-weight: 400;
+             padding: 4px;
+            }
+			 [type=text]{
+             width: 250px;
+             height: 30px;
+             background: transparent;
+             border: 1px solid rgba(255,255,255,0.6);
+             border-radius: 2px;
+             color: #fff;
+             font-family: ."Exo"., sans-serif;
+             font-size: 16px;
+             font-weight: 400;
+             padding: 4px;
+            }
 
+		</style>
 		</head>
             <body bgcolor="#808080" link="black" vlink="black" alink="black" bgcolor="black" text-color="white">
                 <table  width="100%" height="auto" border="2">
@@ -233,7 +275,72 @@ class _Print //Интерфес облочного хранилища
   <meta charset="UTF-8">
   <link href="Access.css" rel="stylesheet">
   <title>Access list</title>
-  
+  <style>
+		
+body{
+	margin: 0;
+	padding: 0;
+	background: #000;
+	color: #fff;
+	font-family: Arial;
+	font-size: 12px;
+}
+
+.body{
+	position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+}
+
+.login button{
+	width: 260px;
+	height: 35px;
+	background: #fff;
+	border: 1px solid #fff;
+	cursor: pointer;
+	border-radius: 2px;
+	color: #000;
+	font-family: "Exo", sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 6px;
+	margin-top: 10px;
+}
+
+.login input[type=button]:hover{
+	opacity: 0.8;
+}
+
+.login input[type=button]:active{
+	opacity: 0.6;
+}
+
+.login input[type=text]:focus{
+	outline: none;
+	border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=password]:focus{
+	outline: none;
+	border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=button]:focus{
+	outline: none;
+}
+
+::-webkit-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+
+::-moz-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+	</style>
 
 </head>
 
@@ -312,5 +419,142 @@ class _Print //Интерфес облочного хранилища
                     
             return $html;
     }
+	
+	private function GetStyleForm()
+	{
+		return 'body{
+                    margin: 0;
+                    padding: 0;
+                    background: #000;
+
+                    color: #fff;
+                    font-family: Arial;
+                    font-size: 12px;
+            }
+
+            .body{
+                    position: absolute;
+                    top: -20px;
+                    left: -20px;
+                    right: -40px;
+                    bottom: -40px;
+                    width: auto;
+                    height: auto;
+            }
+
+            .grad{
+                    position: absolute;
+                    top: -20px;
+                    left: -20px;
+                    right: -40px;
+                    bottom: -40px;
+                    width: auto;
+                    height: auto;
+                    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
+                    z-index: 1;
+                    opacity: 0.7;
+            }
+
+            .header{
+                    position: absolute;
+                    top: calc(50% - 35px);
+                    left: calc(50% - 255px);
+                    z-index: 2;
+            }
+
+            .header div{
+                    float: left;
+                    color: #fff;
+                    font-family: "Exo", sans-serif;
+                    font-size: 32px;
+                    font-weight: 200;
+            }
+
+            .header div span{
+                    color: #5379fa !important;
+            }
+
+            .login{
+                    position: absolute;
+                    top: calc(50% - 75px);
+                    left: calc(50% - 50px);
+                    height: 150px;
+                    width: 350px;
+                    padding: 10px;
+                    z-index: 2;
+            }
+
+            .login input[type=text]{
+                    width: 250px;
+                    height: 30px;
+                    background: transparent;
+                    border: 1px solid rgba(255,255,255,0.6);
+                    border-radius: 2px;
+                    color: #fff;
+                    font-family: "Exo", sans-serif;
+                    font-size: 16px;
+                    font-weight: 400;
+                    padding: 4px;
+            }
+
+            .login input[type=password]{
+                    width: 250px;
+                    height: 30px;
+                    background: transparent;
+                    border: 1px solid rgba(255,255,255,0.6);
+                    border-radius: 2px;
+                    color: #fff;
+                    font-family: "Exo", sans-serif;
+                    font-size: 16px;
+                    font-weight: 400;
+                    padding: 4px;
+                    margin-top: 10px;
+            }
+
+            .login input[type=submit]{
+                    width: 260px;
+                    height: 35px;
+                    background: #fff;
+                    border: 1px solid #fff;
+                    cursor: pointer;
+                    border-radius: 2px;
+                    color: #000;
+                    font-family: "Exo", sans-serif;
+                    font-size: 16px;
+                    font-weight: 400;
+                    padding: 6px;
+                    margin-top: 10px;
+            }
+
+            .login input[type=submit]:hover{
+                    opacity: 0.8;
+            }
+
+            .login input[type=submit]:active{
+                    opacity: 0.6;
+            }
+
+            .login input[type=text]:focus{
+                    outline: none;
+                    border: 1px solid rgba(255,255,255,0.9);
+            }
+
+            .login input[type=password]:focus{
+                    outline: none;
+                    border: 1px solid rgba(255,255,255,0.9);
+            }
+
+            .login input[type=submit]:focus{
+                    outline: none;
+            }
+
+            ::-webkit-input-placeholder{
+               color: rgba(255,255,255,0.6);
+            }
+
+            ::-moz-input-placeholder{
+               color: rgba(255,255,255,0.6);
+            }';
+	}
 }
 ?>
